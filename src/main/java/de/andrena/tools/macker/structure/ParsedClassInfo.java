be792cd06d777a0fcb4a42425271290d53cb3cf7
Ben Romberg <ben.romberg@andrena.de>
2013-05-13 11:30:01 +0200

    remove warnings

--- src/main/java/de/andrena/tools/macker/structure/ParsedClassInfo.java
+++ src/main/java/de/andrena/tools/macker/structure/ParsedClassInfo.java
@@ -20,203 +20,221 @@
 
 package de.andrena.tools.macker.structure;
 
-import javassist.*;
-import net.innig.collect.CompositeMultiMap;
-import net.innig.collect.InnigCollections;
-import net.innig.collect.MultiMap;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+import javassist.ClassPool;
+import javassist.CtBehavior;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.Modifier;
+import net.innig.collect.CompositeMultiMap;
+import net.innig.collect.InnigCollections;
+import net.innig.collect.MultiMap;
 import de.andrena.tools.macker.util.ClassNameTranslator;
 
 /**
  * Class info retrieved from a class file using Javassist.
- *
+ * 
  * @author Ben Romberg
  */
 public class ParsedClassInfo extends AbstractClassInfo {
 
-    private String fullClassName;
-    private boolean isInterface;
-    private boolean isAbstract;
-    private boolean isFinal;
-    private AccessModifier accessModifier;
-    private ClassInfo extendsClass;
-    private Set<ClassInfo> implementsClasses;
-    private MultiMap references;
+	private String fullClassName;
+	private boolean isInterface;
+	private boolean isAbstract;
+	private boolean isFinal;
+	private AccessModifier accessModifier;
+	private ClassInfo extendsClass;
+	private Set<ClassInfo> implementsClasses;
+	private MultiMap references;
 
-    ParsedClassInfo(final ClassManager classManager, final File classFile) throws IOException, ClassParseException {
-        super(classManager);
-        parse(ClassPool.getDefault().makeClass(new FileInputStream(classFile)));
-    }
+	ParsedClassInfo(final ClassManager classManager, final File classFile) throws IOException, ClassParseException {
+		super(classManager);
+		parse(ClassPool.getDefault().makeClass(new FileInputStream(classFile)));
+	}
 
-    ParsedClassInfo(final ClassManager classManager, final InputStream classFileStream)
-            throws IOException, ClassParseException {
-        super(classManager);
-        parse(ClassPool.getDefault().makeClass(classFileStream));
-    }
+	ParsedClassInfo(final ClassManager classManager, final InputStream classFileStream) throws IOException,
+			ClassParseException {
+		super(classManager);
+		parse(ClassPool.getDefault().makeClass(classFileStream));
+	}
 
-    public String getFullName() {
-        return this.fullClassName;
-    }
+	@Override
+	public String getFullName() {
+		return this.fullClassName;
+	}
 
-    public boolean isComplete() {
-        return true;
-    }
+	@Override
+	public boolean isComplete() {
+		return true;
+	}
 
-    public boolean isInterface() {
-        return this.isInterface;
-    }
+	@Override
+	public boolean isInterface() {
+		return this.isInterface;
+	}
 
-    public boolean isAbstract() {
-        return this.isAbstract;
-    }
+	@Override
+	public boolean isAbstract() {
+		return this.isAbstract;
+	}
 
-    public boolean isFinal() {
-        return this.isFinal;
-    }
+	@Override
+	public boolean isFinal() {
+		return this.isFinal;
+	}
 
-    public AccessModifier getAccessModifier() {
-        return this.accessModifier;
-    }
+	@Override
+	public AccessModifier getAccessModifier() {
+		return this.accessModifier;
+	}
 
-    public ClassInfo getExtends() {
-        return this.extendsClass;
-    }
+	@Override
+	public ClassInfo getExtends() {
+		return this.extendsClass;
+	}
 
-    public Set<ClassInfo> getImplements() {
-        return this.implementsClasses;
-    }
+	@Override
+	public Set<ClassInfo> getImplements() {
+		return this.implementsClasses;
+	}
 
-    public MultiMap getReferences() {
-        return this.references;
-    }
+	@Override
+	public MultiMap getReferences() {
+		return this.references;
+	}
 
-    private void parseClassName(CtClass classFile) {
-        this.fullClassName = classFile.getName();
-    }
+	private void parseClassName(CtClass classFile) {
+		this.fullClassName = classFile.getName();
+	}
 
-    private void parseFlags(CtClass classFile) {
-        this.isInterface = classFile.isInterface();
-        this.isAbstract = Modifier.isAbstract(classFile.getModifiers());
-        this.isFinal = Modifier.isFinal(classFile.getModifiers());
-    }
+	private void parseFlags(CtClass classFile) {
+		this.isInterface = classFile.isInterface();
+		this.isAbstract = Modifier.isAbstract(classFile.getModifiers());
+		this.isFinal = Modifier.isFinal(classFile.getModifiers());
+	}
 
-    private void parseAccess(CtClass classFile) {
-        setAccessModifier(translateAccess(classFile.getModifiers()));
-    }
+	private void parseAccess(CtClass classFile) {
+		setAccessModifier(translateAccess(classFile.getModifiers()));
+	}
 
-    private void setAccessModifier(final AccessModifier accessModifier) {
-        this.accessModifier = accessModifier;
-    }
+	private void setAccessModifier(final AccessModifier accessModifier) {
+		this.accessModifier = accessModifier;
+	}
 
-    private AccessModifier translateAccess(int modifiers) {
-        if (Modifier.isPublic(modifiers)) {
-            return AccessModifier.PUBLIC;
-        }
-        if (Modifier.isProtected(modifiers)) {
-            return AccessModifier.PROTECTED;
-        }
-        if (Modifier.isPrivate(modifiers)) {
-            return AccessModifier.PRIVATE;
-        }
-        return AccessModifier.PACKAGE;
-    }
+	private AccessModifier translateAccess(int modifiers) {
+		if (Modifier.isPublic(modifiers)) {
+			return AccessModifier.PUBLIC;
+		}
+		if (Modifier.isProtected(modifiers)) {
+			return AccessModifier.PROTECTED;
+		}
+		if (Modifier.isPrivate(modifiers)) {
+			return AccessModifier.PRIVATE;
+		}
+		return AccessModifier.PACKAGE;
+	}
 
-    private void parseExtends(CtClass classFile) throws ClassParseException {
-        this.extendsClass = getSafeClassInfo(classFile.getClassFile().getSuperclass());
-    }
+	private void parseExtends(CtClass classFile) throws ClassParseException {
+		this.extendsClass = getSafeClassInfo(classFile.getClassFile().getSuperclass());
+	}
 
-    private void parseImplements(CtClass classFile) throws ClassParseException {
-        implementsClasses = new TreeSet<ClassInfo>(new ClassInfoNameComparator());
-        for (String interfaze : classFile.getClassFile().getInterfaces()) {
-            implementsClasses.add(getSafeClassInfo(interfaze));
-        }
-        implementsClasses = Collections.unmodifiableSet(implementsClasses);
-    }
+	private void parseImplements(CtClass classFile) throws ClassParseException {
+		implementsClasses = new TreeSet<ClassInfo>(new ClassInfoNameComparator());
+		for (String interfaze : classFile.getClassFile().getInterfaces()) {
+			implementsClasses.add(getSafeClassInfo(interfaze));
+		}
+		implementsClasses = Collections.unmodifiableSet(implementsClasses);
+	}
 
-    private void parse(CtClass classFile) throws ClassParseException {
-        parseClassName(classFile);
-        parseFlags(classFile);
-        parseAccess(classFile);
-        parseExtends(classFile);
-        parseImplements(classFile);
-        parseReferences(classFile);
-    }
+	private void parse(CtClass classFile) throws ClassParseException {
+		parseClassName(classFile);
+		parseFlags(classFile);
+		parseAccess(classFile);
+		parseExtends(classFile);
+		parseImplements(classFile);
+		parseReferences(classFile);
+	}
 
-    private void addReference(final Reference ref) {
-        getReferences().put(ref.getTo(), ref);
-    }
+	private void addReference(final Reference ref) {
+		getReferences().put(ref.getTo(), ref);
+	}
 
-    private void parseConstantPoolReferences(CtClass classFile) throws ClassParseException {
-        Set<String> classNames = classFile.getClassFile().getConstPool().getClassNames();
-        for (String className : classNames) {
-            addReference(new Reference(this, getSafeClassInfo(className), ReferenceType.CONSTANT_POOL, null, null));
-        }
-    }
+	private void parseConstantPoolReferences(CtClass classFile) throws ClassParseException {
+		@SuppressWarnings("unchecked")
+		Set<String> classNames = classFile.getClassFile().getConstPool().getClassNames();
+		for (String className : classNames) {
+			addReference(new Reference(this, getSafeClassInfo(className), ReferenceType.CONSTANT_POOL, null, null));
+		}
+	}
 
-    private void parseMethodReferences(CtClass classFile) throws ClassParseException {
-        for (CtBehavior behavior : classFile.getDeclaredMethods()) {
-            final AccessModifier methodAccess = translateAccess(behavior.getModifiers());
-            final List<String> paramsAndReturn = ClassNameTranslator.signatureToClassNames(behavior.getSignature());
-            for (final Iterator<String> i = paramsAndReturn.iterator(); i.hasNext(); ) {
-                String refTo = i.next();
-                ClassInfo safeClassInfo = getSafeClassInfo(refTo);
-                ReferenceType referenceType;
-                if (i.hasNext()) {
-                    referenceType = ReferenceType.METHOD_PARAM;
-                } else {
-                    referenceType = ReferenceType.METHOD_RETURNS;
-                }
+	private void parseMethodReferences(CtClass classFile) throws ClassParseException {
+		for (CtBehavior behavior : classFile.getDeclaredMethods()) {
+			AccessModifier methodAccess = translateAccess(behavior.getModifiers());
+			List<String> paramsAndReturn = ClassNameTranslator.signatureToClassNames(behavior.getSignature());
+			for (Iterator<String> i = paramsAndReturn.iterator(); i.hasNext();) {
+				String refTo = i.next();
+				ClassInfo safeClassInfo = getSafeClassInfo(refTo);
+				ReferenceType referenceType;
+				if (i.hasNext()) {
+					referenceType = ReferenceType.METHOD_PARAM;
+				} else {
+					referenceType = ReferenceType.METHOD_RETURNS;
+				}
 
-                addReference(new Reference(this, safeClassInfo, referenceType, behavior.getName(), methodAccess));
-            }
-            if (behavior.getMethodInfo().getExceptionsAttribute() != null) {
-                String[] exceptionNames = behavior.getMethodInfo().getExceptionsAttribute().getExceptions();
-                for (String exceptionName : exceptionNames) {
-                    addReference(new Reference(this, getSafeClassInfo(exceptionName, behavior.getSignature()), ReferenceType.METHOD_THROWS,
-                            behavior.getName(), methodAccess));
-                }
-            }
-        }
-    }
+				addReference(new Reference(this, safeClassInfo, referenceType, behavior.getName(), methodAccess));
+			}
+			if (behavior.getMethodInfo().getExceptionsAttribute() != null) {
+				String[] exceptionNames = behavior.getMethodInfo().getExceptionsAttribute().getExceptions();
+				for (String exceptionName : exceptionNames) {
+					addReference(new Reference(this, getSafeClassInfo(exceptionName, behavior.getSignature()),
+							ReferenceType.METHOD_THROWS, behavior.getName(), methodAccess));
+				}
+			}
+		}
+	}
 
-    private void parseReferences(CtClass classFile) throws ClassParseException {
-        this.references = new CompositeMultiMap(TreeMap.class, HashSet.class);
-        parseConstantPoolReferences(classFile);
-        parseMethodReferences(classFile);
-        parseFieldReferences(classFile);
-        this.references = InnigCollections.unmodifiableMultiMap(getReferences());
-    }
+	private void parseReferences(CtClass classFile) throws ClassParseException {
+		this.references = new CompositeMultiMap(TreeMap.class, HashSet.class);
+		parseConstantPoolReferences(classFile);
+		parseMethodReferences(classFile);
+		parseFieldReferences(classFile);
+		this.references = InnigCollections.unmodifiableMultiMap(getReferences());
+	}
 
-    private void parseFieldReferences(CtClass classFile) throws ClassParseException {
-        CtField[] fields = classFile.getFields();
-        for (CtField field : fields) {
-            List<String> types = ClassNameTranslator.signatureToClassNames(field.getSignature());
-            if (types.size() != 1) {
-                throw new ClassParseException("expected one type for field " + getFullName() + '.'
-                        + field.getName() + "; got: " + types + " (signature is \"" + field.getSignature()
-                        + "\")", classFile);
-            }
-            addReference(new Reference(this, getSafeClassInfo(types.get(0), field.getSignature()),
-                    ReferenceType.FIELD_SIGNATURE, field.getName(), translateAccess(field.getModifiers())));
-        }
-    }
+	private void parseFieldReferences(CtClass classFile) throws ClassParseException {
+		CtField[] fields = classFile.getFields();
+		for (CtField field : fields) {
+			List<String> types = ClassNameTranslator.signatureToClassNames(field.getSignature());
+			if (types.size() != 1) {
+				throw new ClassParseException("expected one type for field " + getFullName() + '.' + field.getName()
+						+ "; got: " + types + " (signature is \"" + field.getSignature() + "\")", classFile);
+			}
+			addReference(new Reference(this, getSafeClassInfo(types.get(0), field.getSignature()),
+					ReferenceType.FIELD_SIGNATURE, field.getName(), translateAccess(field.getModifiers())));
+		}
+	}
 
-    private ClassInfo getSafeClassInfo(final String className) throws ClassParseException {
-        return getSafeClassInfo(ClassNameTranslator.typeConstantToClassName(className), className);
-    }
+	private ClassInfo getSafeClassInfo(final String className) throws ClassParseException {
+		return getSafeClassInfo(ClassNameTranslator.typeConstantToClassName(className), className);
+	}
 
-    private ClassInfo getSafeClassInfo(final String className, final String unparsedClassName)
-            throws ClassParseException {
-        if (!ClassNameTranslator.isJavaIdentifier(className)) {
-            throw new ClassParseException("unable to parse class name / signature: \"" + unparsedClassName
-                    + "\" (got \"" + className + "\")");
-        }
-        return getClassManager().getClassInfo(className);
-    }
+	private ClassInfo getSafeClassInfo(final String className, final String unparsedClassName)
+			throws ClassParseException {
+		if (!ClassNameTranslator.isJavaIdentifier(className)) {
+			throw new ClassParseException("unable to parse class name / signature: \"" + unparsedClassName
+					+ "\" (got \"" + className + "\")");
+		}
+		return getClassManager().getClassInfo(className);
+	}
 }
--- src/main/java/de/andrena/tools/macker/util/ClassNameTranslator.java
+++ src/main/java/de/andrena/tools/macker/util/ClassNameTranslator.java
@@ -17,78 +17,78 @@
  * Place, Suite 330 / Boston, MA 02111-1307 / USA.
  *______________________________________________________________________________
  */
- 
+
 package de.andrena.tools.macker.util;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.regexp.RE;
 import org.apache.regexp.RESyntaxException;
 
-public class ClassNameTranslator
-    {
-    static public boolean isJavaIdentifier(String className)
-        { return legalJavaIdentRE.match(className); }
-    
-    static public List signatureToClassNames(String signature)
-        {
-        List names = new ArrayList();
-        for(int pos = 0; pos < signature.length(); )
-            {
-            String remaining = signature.substring(pos);
-            if(!sigExtractorRE.match(remaining))
-                throw new IllegalArgumentException("Unable to extract type info from: " + remaining);
-            if(sigExtractorRE.getParen(2) != null)
-                names.add((String) primitiveTypeMap.get(sigExtractorRE.getParen(2)));
-            if(sigExtractorRE.getParen(3) != null)
-                names.add(resourceToClassName(sigExtractorRE.getParen(3)));
-            pos += sigExtractorRE.getParenEnd(0);
-            }
-        return names;
-        }
-    
-    static public String typeConstantToClassName(String typeName)
-        {
-        if(arrayExtractorRE.match(typeName))
-            {
-            if(arrayExtractorRE.getParen(2) != null)
-                return (String) primitiveTypeMap.get(arrayExtractorRE.getParen(2));
-            if(arrayExtractorRE.getParen(3) != null)
-                return resourceToClassName(arrayExtractorRE.getParen(3));
-            }
-        return resourceToClassName(typeName);
-        }
-    
-    static public String resourceToClassName(String className)
-        { return slashRE.subst(classSuffixRE.subst(className, ""), ".").intern(); }
-    
-    static public String classToResourceName(String resourceName)
-        { return (dotRE.subst(resourceName, "/") + ".class").intern(); }
-    
-    static private RE classSuffixRE, slashRE, dotRE, arrayExtractorRE, sigExtractorRE, legalJavaIdentRE;
-    static private Map/*<String,String>*/ primitiveTypeMap;
-    static
-        {
-        try {
-            classSuffixRE = new RE("\\.class$");
-            slashRE = new RE("/");
-            dotRE = new RE("\\.");
-            arrayExtractorRE = new RE(        "^(\\[+([BSIJCFDZV])|\\[+L([^;]*);)$");
-            sigExtractorRE   = new RE("^\\(?\\)?(\\[*([BSIJCFDZV])|\\[*L([^;]*);)");
-            legalJavaIdentRE = new RE("^([:javastart:][:javapart:]*)(\\.([:javastart:][:javapart:]*))*$");
-            }
-        catch(RESyntaxException rese)
-            { throw new RuntimeException("Can't initialize ClassNameTranslator: " + rese); } 
-        
-        primitiveTypeMap = new HashMap();
-        primitiveTypeMap.put("B", "byte");
-        primitiveTypeMap.put("S", "short");
-        primitiveTypeMap.put("I", "int");
-        primitiveTypeMap.put("J", "long");
-        primitiveTypeMap.put("C", "char");
-        primitiveTypeMap.put("F", "float");
-        primitiveTypeMap.put("D", "double");
-        primitiveTypeMap.put("Z", "boolean");
-        primitiveTypeMap.put("V", "void");
-        }
-    }
+public class ClassNameTranslator {
+	static public boolean isJavaIdentifier(String className) {
+		return legalJavaIdentRE.match(className);
+	}
+
+	static public List<String> signatureToClassNames(String signature) {
+		List<String> names = new ArrayList<String>();
+		for (int pos = 0; pos < signature.length();) {
+			String remaining = signature.substring(pos);
+			if (!sigExtractorRE.match(remaining))
+				throw new IllegalArgumentException("Unable to extract type info from: " + remaining);
+			if (sigExtractorRE.getParen(2) != null)
+				names.add(primitiveTypeMap.get(sigExtractorRE.getParen(2)));
+			if (sigExtractorRE.getParen(3) != null)
+				names.add(resourceToClassName(sigExtractorRE.getParen(3)));
+			pos += sigExtractorRE.getParenEnd(0);
+		}
+		return names;
+	}
+
+	static public String typeConstantToClassName(String typeName) {
+		if (arrayExtractorRE.match(typeName)) {
+			if (arrayExtractorRE.getParen(2) != null)
+				return primitiveTypeMap.get(arrayExtractorRE.getParen(2));
+			if (arrayExtractorRE.getParen(3) != null)
+				return resourceToClassName(arrayExtractorRE.getParen(3));
+		}
+		return resourceToClassName(typeName);
+	}
+
+	static public String resourceToClassName(String className) {
+		return slashRE.subst(classSuffixRE.subst(className, ""), ".").intern();
+	}
+
+	static public String classToResourceName(String resourceName) {
+		return (dotRE.subst(resourceName, "/") + ".class").intern();
+	}
+
+	static private RE classSuffixRE, slashRE, dotRE, arrayExtractorRE, sigExtractorRE, legalJavaIdentRE;
+	static private Map<String, String> primitiveTypeMap;
+	static {
+		try {
+			classSuffixRE = new RE("\\.class$");
+			slashRE = new RE("/");
+			dotRE = new RE("\\.");
+			arrayExtractorRE = new RE("^(\\[+([BSIJCFDZV])|\\[+L([^;]*);)$");
+			sigExtractorRE = new RE("^\\(?\\)?(\\[*([BSIJCFDZV])|\\[*L([^;]*);)");
+			legalJavaIdentRE = new RE("^([:javastart:][:javapart:]*)(\\.([:javastart:][:javapart:]*))*$");
+		} catch (RESyntaxException rese) {
+			throw new RuntimeException("Can't initialize ClassNameTranslator: " + rese);
+		}
+
+		primitiveTypeMap = new HashMap<String, String>();
+		primitiveTypeMap.put("B", "byte");
+		primitiveTypeMap.put("S", "short");
+		primitiveTypeMap.put("I", "int");
+		primitiveTypeMap.put("J", "long");
+		primitiveTypeMap.put("C", "char");
+		primitiveTypeMap.put("F", "float");
+		primitiveTypeMap.put("D", "double");
+		primitiveTypeMap.put("Z", "boolean");
+		primitiveTypeMap.put("V", "void");
+	}
+}
